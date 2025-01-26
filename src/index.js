d11996b4aeadc1f0177a48fe8196c2544d969303
Luis Klembt <luis.klembt@gmx.de>
2025-01-26 20:06:55 +0100

    !! halb fertig!!! Füge Funktion zum Löschen von Spielern hinzu und implementiere Passwortbestätigung im Modal

--- src/index.js
+++ src/index.js
@@ -20,6 +20,13 @@ function App() {
     const [password, setPassword] = useState("");
     const handleClose = () => setShow(false);
     const [loader, setLoader] = useState(false);
+    const [deletPlayerId, seturDeletePlayerId] = useState("");
+    const [deletPlayerGame, seturDeletePlayerGame] = useState("");
+    const [show2, setShow2] = useState(false);
+    const [modalData2, setModalData2] = useState("");
+    const [password2, setPassword2] = useState("");
+    const handleClose2 = () => setShow2(false);
+    const [loader2, setLoader2] = useState(false);
 
   useEffect(() => {
     onSnapshot(collection(db, "santaClausGame"), (snapshot) => {
@@ -115,6 +122,30 @@ function App() {
     }else return gameData
   };
 
+  const deletePlayer = (id, game, name, rank, score) => {
+    if(id !== "end"){
+      setShow2(true)
+      setModalData2('Willst du wirklich Spieler: "' + name + '" mit dem Score von ' + score + ' und der Platzierung von ' + rank + ' löschen?')
+      seturDeletePlayerId(id);
+      seturDeletePlayerGame(game);
+      return;
+    }
+    if(password2 === "Rosti123!"){
+      setLoader2(true)
+      try {
+        deleteDoc(doc(db, deletPlayerGame, deletPlayerId))
+      } catch (error) {
+        console.error(error)
+      }
+    }else{
+      alert("Falsches Passwort");
+    }
+    setLoader2(false)
+    setShow2(false)
+    setPassword2("")
+    setModalData2("")
+  }
+
   return (
     <div className='mt-2' style={{maxWidth: '90%', margin: 'auto', height: '800px'}}>
       <Modal show={show} onHide={handleClose}>
@@ -122,15 +153,36 @@ function App() {
           <Modal.Title>Löschen von "{modalData}" bestätigen</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            { loader ? <div class="loader">
-          <div class="box1"></div>
-          <div class="box2"></div>
-          <div class="box3"></div>
+            { loader ? <div className="loader">
+          <div className="box1"></div>
+          <div className="box2"></div>
+          <div className="box3"></div>
         </div> : <><FormLabel>Passwort eingeben</FormLabel>
           <FormControl type='password' value={password} onChange={(e) => setPassword(e.target.value)} /></> }
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="danger" onClick={() => deletePlayer("end")}>
+            Löschen bestätigen
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal show={show2} onHide={handleClose2}>
+        <Modal.Header closeButton>
+          <Modal.Title>{modalData2} </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            { loader2 ? <div className="loader">
+          <div className="box1"></div>
+          <div className="box2"></div>
+          <div className="box3"></div>
+        </div> : <><FormLabel>Passwort eingeben</FormLabel>
+          <FormControl type='password' value={password2} onChange={(e) => setPassword2(e.target.value)} /></> }
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose2}>
             Close
           </Button>
           <Button variant="danger" onClick={() => clearGame("end")}>
@@ -146,7 +198,7 @@ function App() {
           <Card.Img variant="top" src="./santaclaus.jpg" />
           <Card.Header>
             <h2 style={{ display: 'inline' }}>Santaclaus</h2>
-            <Badge  onClick={() => clearGame("santaClausGame")} bg="primary" style={{ fontSize: '20px', float: 'right' }}>{santaClausGamePlayer}</Badge>
+            <Badge  onClick={() => clearGame("santaClausGame")} bg="primary" style={{ fontSize: '15px', float: 'right' }}>{santaClausGamePlayer}</Badge>
           </Card.Header>
           <Card.Body>
             {/* <h4 className="mb-4">Es { santaClausGamePlayer === 1  ? "hat" : "haben" } { santaClausGamePlayer === 0 ?  "noch keine" : santaClausGamePlayer } Spieler teilgenommen</h4> */}
@@ -160,13 +212,13 @@ function App() {
               </thead> */}
               <tbody>
                 { getRankings(santaClausGame)?.map((player) => (
-                  <tr key={player.id}>
-                    <td>{player.rank === 1 ? <img alt='' src='./gold.png' style={{ width: '30px' }} /> : null}
-                    {player.rank === 2 ? <img alt='' src='./silber.png' style={{ width: '30px' }} /> : null}
-                    {player.rank === 3 ? <img alt='' src='./bronze.png' style={{ width: '30px' }} /> : null}</td>
-                    <td className={player.rank <= 3 ? 'pulse' : null}>{player.rank}</td>
+                  <tr onClick={() => deletePlayer(player.id, 'santaClausGame', player.name, player.rank, player.score)} key={player.id}>
+                    <td>{player.rank === 1 ? <img alt='' src='./gold.png' style={{ width: '20px' }} /> : null}
+                    {player.rank === 2 ? <img alt='' src='./silber.png' style={{ width: '20px' }} /> : null}
+                    {player.rank === 3 ? <img alt='' src='./bronze.png' style={{ width: '20px' }} /> : null}</td>
+                    <td className={player.rank <= 3 ? 'pulse text-end' : 'text-end'}>{player.rank}.</td>
                     <td className={player.rank <= 3 ? 'pulse' : null}>{player.name}</td>
-                    <td className={player.rank <= 3 ? 'pulse' : null}>{player.score}</td>
+                    <td className={player.rank <= 3 ? 'pulse text-end' : 'text-end'}>{player.score}</td>
                   </tr>
                 ))}
               </tbody>
@@ -179,19 +231,19 @@ function App() {
             <Card.Img variant="top" src="./game-test.jpg" />
             <Card.Header>
               <h2 style={{ display: 'inline' }}>Autospiel</h2>
-              <Badge onClick={() => clearGame("carGame1")} bg="primary" style={{ fontSize: '20px', float: 'right' }}>{carGame1Player}</Badge>
+              <Badge onClick={() => clearGame("carGame1")} bg="primary" style={{ fontSize: '15px', float: 'right' }}>{carGame1Player}</Badge>
             </Card.Header>
             <Card.Body>
               <Table style={{ fontSize: '20px' }}>
                 <tbody>
                   { getRankings(carGame1)?.map((player) => (
-                    <tr key={player.id}>
-                      <td>{player.rank === 1 ? <img alt='' src='./gold.png' style={{ width: '30px' }} /> : null}
-                    {player.rank === 2 ? <img alt='' src='./silber.png' style={{ width: '30px' }} /> : null}
-                    {player.rank === 3 ? <img alt='' src='./bronze.png' style={{ width: '30px' }} /> : null}</td>
-                      <td className={player.rank <= 3 ? 'pulse' : null}>{player.rank}</td>
+                    <tr onClick={() => deletePlayer(player.id, 'carGame1', player.name, player.rank, player.score)} key={player.id}>
+                      <td>{player.rank === 1 ? <img alt='' src='./gold.png' style={{ width: '20px' }} /> : null}
+                    {player.rank === 2 ? <img alt='' src='./silber.png' style={{ width: '20px' }} /> : null}
+                    {player.rank === 3 ? <img alt='' src='./bronze.png' style={{ width: '20px' }} /> : null}</td>
+                      <td className={player.rank <= 3 ? 'pulse text-end' : 'text-end'}>{player.rank}.</td>
                       <td className={player.rank <= 3 ? 'pulse' : null}>{player.name}</td>
-                      <td className={player.rank <= 3 ? 'pulse' : null}>{player.score}</td>
+                      <td className={player.rank <= 3 ? 'pulse text-end' : 'text-end'}>{player.score}</td>
                     </tr>
                   ))}
                 </tbody>
@@ -204,19 +256,19 @@ function App() {
             <Card.Img variant="top" src="./astroids.jpg" />
             <Card.Header>
               <h2 style={{ display: 'inline' }}>Astroids</h2>
-              <Badge onClick={() => clearGame("astroidsGame")} bg="primary" style={{ fontSize: '20px', float: 'right' }}>{astroidsGamePlayer}</Badge>
+              <Badge onClick={() => clearGame("astroidsGame")} bg="primary" style={{ fontSize: '15px', float: 'right' }}>{astroidsGamePlayer}</Badge>
             </Card.Header>
             <Card.Body>
               <Table style={{ fontSize: '20px' }}>
                 <tbody>
                   { getRankings(astroidsGame)?.map((player) => (
-                    <tr key={player.id}>
-                      <td>{player.rank === 1 ? <img alt='' src='./gold.png' style={{ width: '30px' }} /> : null}
-                    {player.rank === 2 ? <img alt='' src='./silber.png' style={{ width: '30px' }} /> : null}
-                    {player.rank === 3 ? <img alt='' src='./bronze.png' style={{ width: '30px' }} /> : null}</td>
-                      <td className={player.rank <= 3 ? 'pulse' : null}>{player.rank}</td>
+                    <tr onClick={() => deletePlayer(player.id, 'astroidsGame', player.name, player.rank, player.score)} key={player.id}>
+                      <td>{player.rank === 1 ? <img alt='' src='./gold.png' style={{ width: '20px' }} /> : null}
+                    {player.rank === 2 ? <img alt='' src='./silber.png' style={{ width: '20px' }} /> : null}
+                    {player.rank === 3 ? <img alt='' src='./bronze.png' style={{ width: '20px' }} /> : null}</td>
+                      <td className={player.rank <= 3 ? 'pulse text-end' : 'text-end'}>{player.rank}.</td>
                       <td className={player.rank <= 3 ? 'pulse' : null}>{player.name}</td>
-                      <td className={player.rank <= 3 ? 'pulse' : null}>{player.score}</td>
+                      <td className={player.rank <= 3 ? 'pulse text-end' : 'text-end'}>{player.score}</td>
                     </tr>
                   ))}
                 </tbody>
